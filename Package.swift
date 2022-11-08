6b240cc259b1553055469d095df7c4c018fbc694
tkachkachishvili <tkach13@freeuni.edu.ge>
2022-11-08 16:21:55 +0400

    initial commit

--- Package.swift
+++ Package.swift
@@ -1,8 +1,6 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let remoteKotlinUrl = "https://tfssrv.hq.tbc/DefaultCollection/112c2bce-6ea4-429d-a839-faf2392b9511/_packaging/TestForKmm/maven/v1/ge/tkach13/sharedlib/shared-kmmbridge/1.0/shared-kmmbridge-1.0.zip"
-let remoteKotlinChecksum = "5c1e4f21fb265ff9e04bc9b4f7d1944c41498bbd2d7a9760a2eeeaa1359c186f"
 let packageName = "shared"
 
 let package = Package(
@@ -19,8 +17,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: packageName,
-            url: remoteKotlinUrl,
-            checksum: remoteKotlinChecksum
+            path: "./shared/build/XCFrameworks/debug/\(packageName).xcframework"
         )
         ,
     ]
